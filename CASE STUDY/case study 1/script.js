cfed27e24cce46e2946199c4b19126b2e64a795f
shubhkamle <me.shubhkamle@gmail.com>
2026-08-06 15:29:16 +0530

    Create script.js

---
+++ CASE STUDY/case study 1/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const welcomeBox = document.getElementById('welcomeMessage');
+  if (welcomeBox) {
+    welcomeBox.textContent = 'Welcome to SIT Nagpur';
+  }
+});
